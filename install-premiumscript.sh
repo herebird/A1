7073ac52131eaa54474a38cef672336d1f0bdf13
herebird <35319775+herebird@users.noreply.github.com>
2018-01-21 19:40:37 +0700

    Update install-premiumscript.sh

--- install-premiumscript.sh
+++ install-premiumscript.sh
@@ -19,7 +19,7 @@ cat > /root/limit.sh <<END3
 END3
 
 cd /usr/local/bin
-wget -O premium-script.tar.gz "https://github.com/lnwshop/vips/blob/master/premium-script.tar.gz"
+wget -O premium-script.tar.gz "https://raw.githubusercontent.com/lnwshop/vips/master/premium-script.tar.gz"
 tar -xvf premium-script.tar.gz
 rm -f premium-script.tar.gz
 
